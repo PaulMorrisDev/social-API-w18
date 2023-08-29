e0c786d9ae2eef2bf0c679161a3d6722fd83d114
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-08-29 01:21:28 +0100

    fixed the time formatting error

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,13 +1,7 @@
 const dayjs = require('dayjs');
-const relativeTime = require('dayjs/plugin/relativeTime');
 
 const dateFormat = (date) => {
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-  };
-  
-  const timeago = (timestamp) => {
-    dayjs.extend(relativeTime);
-    return dayjs().fromNow(timestamp);
-  };
-  
-  module.exports = { dateFormat, timeago };
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+};
+
+module.exports = dateFormat;
